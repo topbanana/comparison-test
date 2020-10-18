638edb84f962e7ee4ec4763e0456209c9f29daad
J B <jonbreen@outlook.com>
2020-10-18 16:00:41 +0100

    Added note about reposirory implementation

--- ComparisonTest/Services/MessageRepository.cs
+++ ComparisonTest/Services/MessageRepository.cs
@@ -5,8 +5,12 @@ using ComparisonTest.Models;
 namespace ComparisonTest.Services
 {
     /// <summary>
-    /// The is an implementation of <see cref="IRepository{MessageEntity}" /> that allows this to run but offers no
-    /// persistence and should be swapped out to allow for a real backend
+    /// The is an implementation of <see cref="IRepository{MessageEntity}" /> that allows this application to run but
+    /// offers no persistence and should be swapped out to allow for a persistent backend. This class is also not under
+    /// test as I would not expect this implementation to go to production. This was added due to time constraints
+    /// and I would expect to wrap an entity-framework data-context and not a list. This is also added as a singleton
+    /// in the IOC container but List is not thread-safe. This is also present to ensure that models passed in are not
+    /// entities and allow for decoration with data-annotations on the dto <see cref="Message" />.
     /// </summary>
     public class MessageRepository : IRepository<MessageEntity>
     {
